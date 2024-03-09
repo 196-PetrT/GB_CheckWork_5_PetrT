d5f7cfca31b3a60b2d37cd529263f1b489d85d58
PetrT <140140070+196-PetrT@users.noreply.github.com>
2024-03-09 16:47:08 +0300

    final ver checkwork_Petr_T

---
+++ src/core/ArrayDataError.java
@@ -0,0 +1,7 @@
+package core;
+
+public class ArrayDataError extends RuntimeException{
+    public ArrayDataError(String message){
+        System.out.println(message);
+    }
+}
---
+++ src/core/PhoneNumberError.java
@@ -0,0 +1,7 @@
+package core;
+
+public class PhoneNumberError extends NumberFormatException{
+    public PhoneNumberError(String message){
+        System.out.println(message);
+    }
+}
